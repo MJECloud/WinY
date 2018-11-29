8a9168bf48a2890f3898573bda41d3dfa698e60c
Max Belohoubek <max.belohoubek@gob.de>
2018-11-29 16:32:45 +0100

    major changes

--- renderer.js
+++ renderer.js
@@ -1,3 +1,19 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const Vue = require('vue/dist/vue.js');
+
+const App = new Vue({
+    el: '#app',
+    data: {
+      title: 'WinY',
+      history: [],
+    },
+  
+    mounted() {
+    },
+  
+    computed: {
+    },
+  
+    methods: {
+    }
+  
+  });
