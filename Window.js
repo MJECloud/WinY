3feaf5c9ef9e24c9dec457ffafcb82a4ebd5d111
Max Belohoubek <m.belohoubek@outlook.com>
2019-05-27 20:50:11 +0200

    additional changes

---
+++ Window.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { BrowserWindow } = require('electron');
+
+const defaultProps = {
+  width: 500,
+  height: 800,
+  show: false
+}
+
+class Window extends BrowserWindow {
+  constructor ({ file, ...windowSettings}) {
+    super({ ...defaultProps, ...windowSettings});
+
+    this.loadFile(file);
+    this.webContents.openDevTools();
+
+    this.once('ready-to-show', () => {
+      this.show();
+    })
+  }
+}
+
+module.exports = Window;
---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { app } = require('electron');
+
+const Window = require('./Window')
+
+function main(){
+  let mainWindow = new Window({
+    file: 'index.html'
+  });
+}
+
+app.on('ready',main);
+
+app.on('window-all-closed', function() {
+  app.quit();
+});
